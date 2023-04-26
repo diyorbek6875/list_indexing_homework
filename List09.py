83eefca9ea573c89b33fd7dc87d4ff67bd206286
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-26 11:33:15 +0500

    9

--- List09.py
+++ List09.py
@@ -6,4 +6,16 @@ def main(list1):
     Returns:
         bool: return answer
     """
-    return
+    a=len(list1)
+    s=0
+    i=0
+    while i<a:
+        if list1[0]==list1[i]:
+            s+=1
+        i+=1
+    if a==s:
+        s=True
+    else:
+        s=False
+    return s
+print(main([1,1,1,1]))
