65642542884b0c4a155b273c85e1e3010f79f32a
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-26 10:56:09 +0500

    6

--- List06.py
+++ List06.py
@@ -6,4 +6,12 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    i=0
+    while i<len(list1):
+        if list1[i]==1:
+            list1[i]=True
+        i=i+1
+
+    return list1
+print(main([1,2,3,4,5]))
+
